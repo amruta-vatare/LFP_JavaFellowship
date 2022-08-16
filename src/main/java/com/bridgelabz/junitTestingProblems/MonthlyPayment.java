e7c5c61d154a7c1fede8a438a26b6d4a55501812
amruta.vatare <vatareamruta615@gmail.com>
2022-08-16 18:36:58 +0530

    Monthly payment problem solved

--- src/main/java/com/bridgelabz/junitTestingProblems/MonthlyPayment.java
+++ src/main/java/com/bridgelabz/junitTestingProblems/MonthlyPayment.java
@@ -7,18 +7,20 @@ import java.util.Scanner;
 public class MonthlyPayment {
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
-        System.out.println("Enter the Year to pay off");
+        System.out.println("Enter Year to pay off");
         int Y = sc.nextInt();
-        System.out.println("Enter the Principle loan amount ");
+        System.out.println("Enter Principle loan amount ");
         int P = sc.nextInt();
-        System.out.println("Enter the Compounded monthly interest rate");
+        System.out.println("Enter monthly interest rate");
         int R = sc.nextInt();
         calculateMonthlyPayment(Y,P,R);
     }
 
     private static void calculateMonthlyPayment(int Y, int P, int R) {
-        int n = 12 * Y;
-        int r = R/(12*100);
+        double n = 12 * Y;
+        double r = R / (12 * 100);
+        double payment = P * r / (1 - Math.pow((1 + r), -n));
+        System.out.println("Monthly Payment is :"+payment);
 
     }
 }
